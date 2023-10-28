123e839924930ab64b3a4a20f165f363da67e946
Pierre Amelot <pierre.p.amelot@gmail.com>
2023-10-28 20:57:15 +0200

    Move app files to sandbox

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./sandbox/**/*.{html,ts,tsx}"],
   theme: {
     extend: {},
   },
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    open: "/sandbox/",
+  },
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points

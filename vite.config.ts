9540ad94cde4772d41a31cb5bd414590098f0d35
Pierre Amelot <pierre.p.amelot@gmail.com>
2023-10-29 02:16:24 +0200

    Tweak Vite config
    * Add sourcemap for production
    * Remove deps and peerDeps from build output

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,8 @@ export default defineConfig({
     postcss: 'sandbox',
   },
   build: {
+    // View sourcemap in browser devtools when using the library
+    sourcemap: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: path.resolve(
@@ -26,10 +28,26 @@ export default defineConfig({
       fileName: 'remote-images-for-next',
     },
     rollupOptions: {
-      external: [],
+      // Since this is a modern component library, we can rely on the fact that
+      // the application using it will itself install and bundle the 3rd party
+      // dependency. This will avoid duplicates.
+      // https://github.com/vitejs/vite/discussions/6198
+      external: [
+        ...Object.keys(pkg.peerDependencies || {}),
+        ...Object.keys(pkg.dependencies || {}),
+        // We also add 'react/jsx-runtime' here because it will be used by Vite
+        // React plugin (for the automatic JSX runtime), but since react is a
+        // peer dependency the app will have access to it so we don't need it in
+        // the bundle
+        'react/jsx-runtime',
+      ],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
+        // Provide global variables to use in the UMD build for externalized
+        // deps. This will avoid Rollup warnings when bundling the UMD modules,
+        // but in practice it shouldn't really matter because these globals
+        // would only be used if someone were to import our library as an iife
+        // module and rely on react and react-dom to be provided as properties
+        // of our global object (in our case global.React and global.ReactDOM)
         globals: {
           react: 'react',
         },
